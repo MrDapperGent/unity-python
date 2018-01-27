2c93914cab357313dbf02663d88450ee1130b38b
Sean Loper <grimmdev@gmail.com>
2018-01-26 19:03:00 -0500

    Added TextAsset Demo and upgraded IronPython to 2.7.7

---
+++ Plugins/UnityPython/Examples/Code/ScriptLoader.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Microsoft.Scripting.Hosting;
+using System.Collections;
+
+public class ScriptLoader : MonoBehaviour {
+
+	public TextAsset scriptFile;
+	public bool OnAwake = true;
+
+	private ScriptEngine thisEngine;
+	private ScriptScope thisScope;
+	private ScriptSource thisSource;
+
+	private void Awake()
+	{
+		if (OnAwake)
+			Init ();
+	}
+
+	private void Init()
+	{
+		thisEngine = global::UnityPython.CreateEngine();
+		thisScope = thisEngine.CreateScope();
+		thisSource = thisEngine.CreateScriptSourceFromString(scriptFile.text);
+		thisSource.Execute(thisScope);
+	}
+}
